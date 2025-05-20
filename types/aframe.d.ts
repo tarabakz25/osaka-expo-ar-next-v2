8ca21f5330935951093983cea1a6be20eb61afaf
Kz <aikikizuki@gmail.com>
2025-05-20 18:42:08 +0900

    ARシーンの型定義を追加し、package.jsonに@types/aframeを追加。tsconfig.jsonの型設定を更新。

--- types/aframe.d.ts
+++ types/aframe.d.ts
@@ -1,9 +1,14 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    'a-scene': any;
-    'a-assets': any;
-    'a-marker': any;
-    'a-entity': any;
-    'a-video': any;
+/// <reference types="aframe" />
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'a-scene': any;
+      'a-assets': any;
+      'a-asset-item': any;
+      'a-marker': any;
+      'a-entity': any;
+      'a-video': any;
+    }
   }
 } 
